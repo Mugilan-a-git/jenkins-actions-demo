84053e16cc0dab9e884d150d8c8ad466e39c50e9
Mugilan A <mugilana@dataswitch.co>
2026-09-21 16:58:10 +0530

    Initial React Hello World application

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="card">
+        <h1>Hello World</h1>
+        <p>Jenkins CI/CD Demo</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
